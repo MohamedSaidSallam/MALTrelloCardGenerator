9dfd6ace12f7aa2dab2b13837feede936e357313
TheDigitalPhoenixX <MohamedSaidSallam@outlook.com>
2021-10-01 16:29:48 +0200

    feat: get MAL data

--- popup.js
+++ popup.js
@@ -1,24 +1,49 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+const getDataButton = document.getElementById("getData");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+getDataButton.addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: getData,
+    },
+    ([{ result }]) => {
+      alert(JSON.stringify(result));
+    }
+  );
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+function getData() {
+  const data = {
+    title: document.querySelector(
+      "#contentWrapper > div:nth-child(1) > div > div.h1-title > div > h1 > strong"
+    ).textContent,
+    englishTitle: (
+      document.querySelector(
+        "#contentWrapper > div:nth-child(1) > div > div.h1-title > div > p"
+      ) || {}
+    ).textContent,
+    score: document.querySelector(
+      "#content > table > tbody > tr > td:nth-child(2) > div.js-scrollfix-bottom-rel > table > tbody > tr:nth-child(1) > td > div.pb16 > div.di-t.w100.mt12 > div.anime-detail-header-stats.di-tc.va-t > div.stats-block.po-r.clearfix > div.fl-l.score > div"
+    ).textContent,
+    imageURL: document
+      .querySelector(
+        "#content > table > tbody > tr > td.borderClass > div > div:nth-child(1) > a > img"
+      )
+      .getAttribute("src"),
+    malURL: window.location.href,
+  };
+  data.epCount = document
+    .querySelector(
+      `#content > table > tbody > tr > td.borderClass > div > div:nth-child(${
+        data.englishTitle ? 14 : 13
+      })`
+    )
+    .textContent // '\n  Episodes:\n  12\n  '
+    .trim() // 'Episodes:\n  12'
+    .split("\n") //['Episodes:', '  12']
+    .at(-1) //' 12'
+    .trim(); //'12'
+  return data;
 }
